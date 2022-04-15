f97685d3c53a029d3f41075bb4bf763ebf523487
Jean-Antoine <maro_j@etna-alternance.net>
2022-04-15 10:32:36 +0200

    add: resources, main header, main, readline, putchar, Makefile

---
+++ include/main_header.h
@@ -0,0 +1,18 @@
+/*
+** ETNA PROJECT, 15/04/2022 by charda_p, maro_j, pondav_j
+** File description:
+**      main header
+*/
+
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+#include "Boss.h"
+#include "Enemy.h"
+#include "Players.h"
+#include "rpg.h"
+
+void my_putchar(char c);
+char *my_readline(void);
+
+#endif
